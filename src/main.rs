a65b27d6ca19781b26426eb55d1a4ba6dcd1bc10
Nesterov Nikita <shadr.nn@gmail.com>
2024-03-17 10:14:52 +0300

    refactor: run different function depending on file extension

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ pub mod utils;
 use dat::DatFile;
 use dat_schema::SchemaFile;
 use poefs::{local::LocalSource, online::OnlineSource, PoeFS};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::Parser;
 
@@ -34,14 +34,16 @@ pub enum Command {
     ListPaths,
 }
 
-fn get_file(fs: &mut PoeFS, path: PathBuf, output: PathBuf) -> Result<(), anyhow::Error> {
-    let file_name = path.file_stem().unwrap().to_str().unwrap();
-    let file_bytes = fs.get_file(path.to_str().unwrap())?.unwrap();
-    let file_dat = DatFile::new(file_bytes);
+fn get_dat_file(
+    bytes: Vec<u8>,
+    table_name: &str,
+    output: impl AsRef<Path>,
+) -> Result<(), anyhow::Error> {
+    let file_dat = DatFile::new(bytes);
 
     let schema_content = std::fs::read_to_string("schema.min.json")?;
     let schema: SchemaFile = serde_json::from_str(&schema_content)?;
-    let file_schema = schema.find_table(file_name).unwrap();
+    let file_schema = schema.find_table(table_name).unwrap();
     let file_columns = &file_schema.columns;
 
     let mut wtr = csv::Writer::from_path(output)?;
@@ -62,6 +64,23 @@ fn get_file(fs: &mut PoeFS, path: PathBuf, output: PathBuf) -> Result<(), anyhow
         wtr.write_record(values)?;
     }
     wtr.flush()?;
+    Ok(())
+}
+
+fn get_file(fs: &mut PoeFS, path: PathBuf, output: PathBuf) -> Result<(), anyhow::Error> {
+    let extension = path.extension().unwrap().to_str().unwrap();
+    let file_name = path.file_stem().unwrap().to_str().unwrap();
+    let file_bytes = fs.get_file(path.to_str().unwrap())?.unwrap();
+
+    match extension {
+        "dat64" => {
+            get_dat_file(file_bytes, file_name, output)?;
+        }
+        _ => unimplemented!(
+            "Reading files with extension: {} not supported yet",
+            extension
+        ),
+    }
 
     Ok(())
 }
