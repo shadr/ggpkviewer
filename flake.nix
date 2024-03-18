f36207731178597f7eb1276529d27ce50270df8b
Nesterov Nikita <shadr.nn@gmail.com>
2024-03-18 12:41:26 +0300

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,16 @@
 {
-  description = "A basic Rust devshell";
-
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      with pkgs;
-      {
+        pkgs = import nixpkgs { inherit system overlays; };
+      in with pkgs; {
         devShells.default = mkShell {
           buildInputs = [
             openssl
@@ -25,13 +19,6 @@
             zlib.out
             rust-bin.stable.latest.default
           ];
-
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
-            alias grep=ripgrep
-            '';
         };
-      }
-    );
+      });
 }
